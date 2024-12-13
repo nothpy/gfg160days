35dee93ff3c22b02b7923dbfa0de148bc2a8419b
karan <145522733+nothpy@users.noreply.github.com>
2024-12-13 21:28:26 +0530

    Create Day-26 Sorted and Rotated Minimum.cpp

---
+++ Day-26 Sorted and Rotated Minimum.cpp
@@ -0,0 +1,12 @@
+int findMin(vector<int>& arr) 
+    {
+        int left=0, right=arr.size()-1;
+        
+        while(left<right)
+        {
+            int mid = left +(right-left)/2;
+            if(arr[mid]>arr[right]) left = mid+1;
+            else right = mid;
+        }
+        return arr[left];
+    }
