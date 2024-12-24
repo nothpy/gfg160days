6ac347dc25aa371a6f891e30e78b9a7601f76132
karan <145522733+nothpy@users.noreply.github.com>
2024-12-24 09:32:30 +0530

    Create Day-37 Search in a sorted Matrix.cpp

---
+++ Day-37 Search in a sorted Matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    // Function to search a given number in row-column sorted matrix.
+    bool searchMatrix(vector<vector<int>> &mat, int x) 
+    {
+        int i = 0, n = mat.size(), m = mat[0].size();
+        int j=m-1;
+        while(i<n)
+        {
+            if(mat[i][j]==x) return true;
+            else if(mat[i][j] > x) j--;
+            else if(mat[i][j] < x){
+                i++;
+                j=m-1;
+            }
+        }
+        return false;
+    }
+};
