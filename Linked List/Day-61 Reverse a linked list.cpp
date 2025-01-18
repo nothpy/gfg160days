e96f2e2d19c66f0e817bd0130909e07881adc73e
karan <145522733+nothpy@users.noreply.github.com>
2025-01-18 23:10:05 +0530

    Create Day-61 Reverse a linked list.cpp

---
+++ Linked List/Day-61 Reverse a linked list.cpp
@@ -0,0 +1,27 @@
+/* Linked List Node structure:
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+
+*/
+
+class Solution {
+  public:
+    Node* reverseList(struct Node* head) {
+        Node * curr, *prev, *next;
+        curr = head;
+        prev = NULL;
+        next = curr->next;
+        while(curr)
+        {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+};
