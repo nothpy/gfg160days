9d12d75bb50a99f4a0c01963d0188fceff25fab4
karan <145522733+nothpy@users.noreply.github.com>
2025-03-11 21:58:43 +0530

    Create 8 Ways to Reach the n'th Stair.cpp

---
+++ Dynamic Programming/8 Ways to Reach the n'th Stair.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+  public:
+    int countWays(int n) {
+         if (n == 1) return 1;
+    if (n == 2) return 2;
+    
+    int prev2 = 1, prev1 = 2, curr;
+    
+    for (int i = 3; i <= n; i++) {
+        curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    
+    return curr;
+        
+    }
+};
